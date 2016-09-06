e0eaf286e0285cca15fee0d742abbf9ae05ab6f8
pces1120 <pces1120@gmail.com>
2016-09-06 12:58:03 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+var numero1 = parseInt(prompt('Ingrese un numero'));
+var numero2 = parseInt(prompt('Ingrese otro numero'));
+
+alert('La suma de ambos digitos es:' + ' ' +(numero1 + numero2));
+alert('La resta de ambos digitos es:' + ' ' +(numero1 - numero2));
+alert('La multiplicacion de ambos digitos es:' + ' ' +(numero1 * numero2));
+alert('La division de ambos digitos es:' + ' ' +(numero1 / numero2));
+
+
